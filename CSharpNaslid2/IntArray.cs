ef328e70103a48928a186bff9c2e683e3f054808
Pain31415 <Cherniy412@ukr.net>
2024-03-03 13:37:28 +0200

    Add project files.

---
+++ CSharpNaslid2/IntArray.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpNaslid2
+{
+    public class IntArray : IOutput, IMath, ISort, ICalc
+    {
+        private int[] elements;
+
+        public IntArray(int[] elements)
+        {
+            this.elements = elements;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Елементи масиву:");
+            foreach (int element in elements)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        public void Show(string info)
+        {
+            Console.WriteLine("Елементи масиву:");
+            foreach (int element in elements)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine($"Інформаційне повідомлення: {info}");
+        }
+
+        public int Max()
+        {
+            int max = elements[0];
+            foreach (int element in elements)
+            {
+                if (element > max)
+                {
+                    max = element;
+                }
+            }
+            return max;
+        }
+
+        public int Min()
+        {
+            int min = elements[0];
+            foreach (int element in elements)
+            {
+                if (element < min)
+                {
+                    min = element;
+                }
+            }
+            return min;
+        }
+
+        public float Avg()
+        {
+            int sum = 0;
+            foreach (int element in elements)
+            {
+                sum += element;
+            }
+            return (float)sum / elements.Length;
+        }
+
+        public bool Search(int valueToSearch)
+        {
+            foreach (int element in elements)
+            {
+                if (element == valueToSearch)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void SortAsc()
+        {
+            Array.Sort(elements);
+        }
+
+        public void SortDesc()
+        {
+            Array.Sort(elements);
+            Array.Reverse(elements);
+        }
+
+        public void SortByParam(bool isAsc)
+        {
+            if (isAsc)
+            {
+                SortAsc();
+            }
+            else
+            {
+                SortDesc();
+            }
+        }
+
+        public int Less(int valueToCompare)
+        {
+            int count = 0;
+            foreach (int element in elements)
+            {
+                if (element < valueToCompare)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public int Greater(int valueToCompare)
+        {
+            int count = 0;
+            foreach (int element in elements)
+            {
+                if (element > valueToCompare)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ CSharpNaslid2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+
+namespace CSharpNaslid2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] elements = { 1, 2, 3, 4, 5 };
+            IntArray intArray = new IntArray(elements);
+
+            Console.WriteLine($"Кількість елементів менших за 3: {intArray.Less(3)}");
+            Console.WriteLine($"Кількість елементів більших за 3: {intArray.Greater(3)}");
+        }
+    }
+}

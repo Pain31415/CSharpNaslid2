009567c2509df7b9ab2604d8142f610a6ce85621
Pain31415 <Cherniy412@ukr.net>
2024-03-03 13:37:28 +0200

    Add project files.

---
+++ CSharpNaslid2/IntArray.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpNaslid2
+{
+    public class IntArray : IOutput2
+    {
+        private int[] elements;
+
+        public IntArray(int[] elements)
+        {
+            this.elements = elements;
+        }
+
+        public void ShowEven()
+        {
+            Console.WriteLine("Парні елементи масиву:");
+            foreach (int element in elements)
+            {
+                if (element % 2 == 0)
+                {
+                    Console.WriteLine(element);
+                }
+            }
+        }
+
+        public void ShowOdd()
+        {
+            Console.WriteLine("Непарні елементи масиву:");
+            foreach (int element in elements)
+            {
+                if (element % 2 != 0)
+                {
+                    Console.WriteLine(element);
+                }
+            }
+        }
+    }
+}
---
+++ CSharpNaslid2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CSharpNaslid2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] elements = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            IntArray intArray = new IntArray(elements);
+
+            Console.WriteLine("Парні елементи масиву:");
+            intArray.ShowEven();
+
+            Console.WriteLine("\nНепарні елементи масиву:");
+            intArray.ShowOdd();
+        }
+    }
+}

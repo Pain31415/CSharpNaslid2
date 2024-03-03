328b0bbbf37cbae84ee959f0afc672e914f32c56
Pain31415 <Cherniy412@ukr.net>
2024-03-03 13:37:28 +0200

    Add project files.

---
+++ CSharpNaslid2/IntArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpNaslid2
+{
+    public class IntArray : ICalc2
+    {
+        private int[] elements;
+
+        public IntArray(int[] elements)
+        {
+            this.elements = elements;
+        }
+
+        public int CountDistinct()
+        {
+            return elements.Distinct().Count();
+        }
+
+        public int EqualToValue(int valueToCompare)
+        {
+            int count = 0;
+            foreach (int element in elements)
+            {
+                if (element == valueToCompare)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ CSharpNaslid2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CSharpNaslid2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] elements = { 1, 2, 3, 4, 5, 2, 3, 4, 5, 6 };
+            IntArray intArray = new IntArray(elements);
+
+            Console.WriteLine($"Кількість унікальних значень: {intArray.CountDistinct()}");
+
+            int valueToCompare = 3;
+            Console.WriteLine($"Кількість елементів, рівних {valueToCompare}: {intArray.EqualToValue(valueToCompare)}");
+        }
+    }
+}

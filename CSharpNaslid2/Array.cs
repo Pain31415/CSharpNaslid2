810344628bd661e6fe8c7eca87865d5291623621
Pain31415 <Cherniy412@ukr.net>
2024-03-03 13:37:28 +0200

    Add project files.

---
+++ CSharpNaslid2/Array.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpNaslid2
+{
+    public class Array : IOutput
+    {
+        private int[] elements;
+
+        public Array(int[] elements)
+        {
+            this.elements = elements;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Елементи масиву:");
+            foreach (int element in elements)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        public void Show(string info)
+        {
+            Console.WriteLine("Елементи масиву:");
+            foreach (int element in elements)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine($"Інформаційне повідомлення: {info}");
+        }
+    }
+}
---
+++ CSharpNaslid2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CSharpNaslid2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] elements = { 1, 2, 3, 4, 5 };
+            Array array = new Array(elements);
+
+            Console.WriteLine("Виклик методу Show():");
+            array.Show();
+
+            Console.WriteLine("\nВиклик методу Show(string info):");
+            array.Show("Це інформаційне повідомлення.");
+        }
+    }
+}
